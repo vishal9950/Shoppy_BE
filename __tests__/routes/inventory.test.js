19cceb41e26d3f412e709251d5ab21aa3de02c19
Vishal Vasnani <vishalvasnani123@gmail.com>
2018-03-15 12:17:32 +0530

    Test GET /inventory to return statusCode 200 if data already in DB

--- __tests__/routes/inventory.test.js
+++ __tests__/routes/inventory.test.js
@@ -3,13 +3,31 @@ const Models = require('../../models');
 
 jest.setTimeout(10000);
 describe('Test GET /inventory: ', () => {
-  beforeAll((done) => {
+  beforeEach((done) => {
     Models.inventory.destroy({ truncate: true }).then(() => { done(); });
   });
   test('Should return statusCode 201 if DB empty: ', (done) => {
     Server.inject('/inventory', (response) => {
       expect(response.statusCode).toBe(201);
       done();
+    });
+  });
+
+  test('Should return statusCode 200 if DB is not empty: ', (done) => {
+    Models.inventory.create({
+      item_id: 1,
+      category: 'Fruits',
+      brand: 'Fresho',
+      title: 'Banana',
+      availableqty: 3,
+      cost: 10,
+      description: '1 kg',
+      imageurl: 'http://someurl',
+    }).then(() => {
+      Server.inject('/inventory', (response) => {
+        expect(response.statusCode).toBe(200);
+        done();
+      });
     });
   });
 });
