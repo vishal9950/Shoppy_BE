05839324988ac116eefedfec97a13bae779f6727
Vishal Vasnani <vishalvasnani123@gmail.com>
2018-03-15 15:50:10 +0530

    Test GET /orders to return the ordered items for an existing order_id

--- __tests__/routes/orders.test.js
+++ __tests__/routes/orders.test.js
@@ -30,9 +30,34 @@ describe('Test server for GET /orders: ', () => {
       Models.itemsordered.destroy({ truncate: true }).then(() => { done(); });
     });
   });
+
   test('Should return the statusCode 200 for a valid order_id: ', (done) => {
     Server.inject('/orders', (response) => {
       expect(response.statusCode).toBe(200);
+      done();
+    });
+  });
+
+  test('Should return the orders for a valid order_id: ', (done) => {
+    const testArr = [
+      {
+        order_id: 12,
+        item_id: 10,
+        quantity: 2,
+      },
+      {
+        order_id: 12,
+        item_id: 11,
+        quantity: 1,
+      },
+      {
+        order_id: 12,
+        item_id: 20,
+        quantity: 3,
+      },
+    ];
+    Server.inject('/orders', (response) => {
+      expect(JSON.parse(response.payload)).toEqual(testArr);
       done();
     });
   });
